43573aadd4af13a62c5525148083da8b82ae3756
yesroh <ronaizacard@gmail.com>
2017-05-08 11:42:48 -0300

    Adding mutate to our addChanell

--- client/src/AddChannel.js
+++ client/src/AddChannel.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { gql, graphql } from 'react-apollo'
 
-const addChannel = () => {
+const addChannel = ({ mutate }) => {
   const handleKeyUp = evt => {
     if (evt.keyCode === 13) {
-      console.log(evt.target.value)
-      evt.target.value = ''
+      evt.persist()
+      mutate({
+        variables: { name: evt.target.value }
+      })
+      .then( res => {
+        evt.target.value = ''
+      })
     }
   }
   
@@ -13,4 +19,14 @@ const addChannel = () => {
   )
 }
 
-export default addChannel
+const addChannelMutation = gql `
+  mutation addChannel($name: String!) {
+    addChannel(name: $name) {
+      id
+      name
+    }
+  }
+`
+
+const addChannelWithMutation = graphql(addChannelMutation)(addChannel)
+export default addChannelWithMutation
