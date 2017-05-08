cb379ef90ba80ed7acd0fa3b5dd501d1d2646555
yesroh <ronaizacard@gmail.com>
2017-05-08 10:38:30 -0300

    Discovering by force how to comment in a graphql file

--- client/src/schema.js
+++ client/src/schema.js
@@ -1,14 +1,14 @@
 export const typeDefs = `
   type Channel {
-    id:   ID!  // '!' denotes a required field
+    id:   ID!  # '!' denotes a required field
     name: String
   }
   
-  /**
-  *  This type specifies the entry points into our API. In this case there is only
-  *  one - 'channels' - which will returns a list of channels
-  **/
+  
+  # This type specifies the entry points into our API. In this case there is only
+  # one - 'channels' - which will returns a list of channels
+  
   type Query {
-    channels: [Channel] // [] means that is a list of channels
+    channels: [Channel] # [] means that is a list of channels
   }
 `
