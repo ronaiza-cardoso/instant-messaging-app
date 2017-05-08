f1a5977244e9a0d1bd88b40a44f310df71e2b34e
yesroh <ronaizacard@gmail.com>
2017-05-08 11:55:37 -0300

    Making thing more organized

--- client/src/App.js
+++ client/src/App.js
@@ -3,44 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import {
   ApolloClient,
-  gql,
-  graphql,
   ApolloProvider,
   createNetworkInterface,
 } from 'react-apollo';
 
-import AddChannel from './AddChannel'
+import ChannelsListWithData from './components/ChannelsListWithData';
 
-const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:4000/graphql',
-})
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' })
 
 const client = new ApolloClient({
-  networkInterface,
-});
-
-const ChannelsList = ({ data: { loading, error, channels }}) => {
-  if (loading) {
-    return <p>Loading...</p>
-  }
-  if (error) {
-    return <p>{error.message}</p>
-  }
-  return <ul>
-    <AddChannel />
-    {channels.map (ch => <li key={ch.id}>{ch.name}</li>)}
-  </ul>
-}
-
-const channelsListQuery = gql `
-  query ChannelsListQuery {
-    channels {
-      id
-      name
-    }
-  } 
-`
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList)
+  networkInterface
+})
 
 class App extends Component {
   render() {
