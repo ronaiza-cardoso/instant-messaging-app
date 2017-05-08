f996275072345d3e9510565903fc99dc04366932
yesroh <ronaizacard@gmail.com>
2017-05-08 10:36:46 -0300

    Finish mock to our data

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,21 @@ import {
   ApolloProvider,
 } from 'react-apollo';
 
-const client = new ApolloClient();
+import { 
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} from 'graphql-tools';
+
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
+
+const schema = makeExecutableSchema({ typeDefs })
+addMockFunctionsToSchema({ schema })
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema })
+
+const client = new ApolloClient({
+  networkInterface: mockNetworkInterface
+});
 
 const ChannelsList = ({ data: { loading, error, channels }}) => {
   if (loading) {
